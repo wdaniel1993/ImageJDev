a2969ff17e2398f9b3de93dbc23c4c9682f2ec5d
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-09-27 23:05:42 +0200

    Refactoring

---
+++ plugins/sig1/filter/AbstractBaseFilter.java
@@ -0,0 +1,58 @@
+package filter;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import utility.ByteImage2D;
+import utility.Image2D;
+import utility.Image2DUtility;
+import utility.ImageJUtility;
+
+public abstract class AbstractBaseFilter implements PlugInFilter {
+	
+	@Override
+	public void run(ImageProcessor ip) {
+		byte[] pixels = (byte[]) ip.getPixels();
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+
+		final Image2D inputImage = new ByteImage2D(pixels, width, height);
+		final Image2D outputImage = new ByteImage2D(pixels, width, height);
+
+		GenericDialog gd = new GenericDialog("User Input");
+		prepareDialog(gd);
+		gd.showDialog();
+		if (gd.wasCanceled()) {
+			return;
+		}
+		readDialogResult(gd);
+		
+		processImage(inputImage, outputImage);
+		
+		byte[] outPixels = Image2DUtility.convertFromImage2D(outputImage);
+		ImageJUtility.showNewImage(outPixels, width, height,getFilterName());
+	}
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about")) {
+			showAbout();
+			return DONE;
+		}
+		return DOES_8G + SUPPORTS_MASKING;
+	}
+
+	private void showAbout() {
+		IJ.showMessage("About " + getFilterName() + "...", "this is a PluginFilter template\n");
+	}
+	
+	public abstract void processImage(final Image2D inputImage, final Image2D outputImage);
+	
+	protected abstract String getFilterName();
+	
+	protected abstract void readDialogResult(GenericDialog gd);
+
+	protected abstract void prepareDialog(GenericDialog gd);
+}
--- plugins/sig1/filter/AbstractMaskFilter.java
+++ plugins/sig1/filter/AbstractMaskFilter.java
@@ -4,39 +4,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import ij.IJ;
-import ij.ImagePlus;
 import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
-import ij.process.ImageProcessor;
-import utility.ByteImage2D;
 import utility.Image2D;
-import utility.Image2DUtility;
-import utility.ImageJUtility;
 import utility.Point;
 
-public abstract class AbstractMaskFilter implements PlugInFilter {
+public abstract class AbstractMaskFilter extends AbstractBaseFilter {
 
 	private int radius = 1;
 	private int threadCount = 4;
-
-	@Override
-	public void run(ImageProcessor ip) {
-		byte[] pixels = (byte[]) ip.getPixels();
-		int width = ip.getWidth();
-		int height = ip.getHeight();
-
-		final Image2D inputImage = new ByteImage2D(pixels, width, height);
-		final Image2D outputImage = new ByteImage2D(pixels, width, height);
-
-		GenericDialog gd = new GenericDialog("User Input");
-		prepareDialog(gd);
-		gd.showDialog();
-		if (gd.wasCanceled()) {
-			return;
-		}
-		readDialogResult(gd);
-
+	
+	public void processImage(final Image2D inputImage, final Image2D outputImage) {
 		final List<Thread> threads = new ArrayList<Thread>();
 		final Iterator<Point<Integer>> iterator = inputImage.pointIterator();
 
@@ -59,24 +36,7 @@ public abstract class AbstractMaskFilter implements PlugInFilter {
 		}
 
 		startAndJoin(threads);
-
-		byte[] outPixels = Image2DUtility.convertFromImage2D(outputImage);
-		ImageJUtility.showNewImage(outPixels, width, height,
-				getFilterName() + " calculated with radius r = " + this.radius);
 	}
-
-	@Override
-	public int setup(String arg, ImagePlus imp) {
-		if (arg.equals("about")) {
-			showAbout();
-			return DONE;
-		}
-		return DOES_8G + SUPPORTS_MASKING;
-	}
-
-	private void showAbout() {
-		IJ.showMessage("About " + getFilterName() + "...", "this is a PluginFilter template\n");
-	} // showAbout
 
 	private static void startAndJoin(List<Thread> threads) {
 		for (int ithread = 0; ithread < threads.size(); ithread++) {
@@ -99,8 +59,6 @@ public abstract class AbstractMaskFilter implements PlugInFilter {
 	public int getMaxMaskFields() {
 		return (this.radius * 2 + 1) * (this.radius * 2 + 1);
 	}
-
-	protected abstract String getFilterName();
 
 	protected abstract int transformImagePoint(int x, int y, Image2D mask);
 	
