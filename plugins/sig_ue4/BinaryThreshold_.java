09121ea8d8550a45234357afda967fe4b284369d
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-11-11 20:11:40 +0100

    BinaryThreshold

---
+++ plugins/sig_ue4/BinaryThreshold_.java
@@ -0,0 +1,50 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import ue4.utility.ImageJUtility;
+
+public class BinaryThreshold_ implements PlugInFilter {
+
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about"))
+			{showAbout(); return DONE;}
+		return DOES_8G+SUPPORTS_MASKING;
+	} //setup
+
+	public void run(ImageProcessor ip) {
+		byte[] pixels = (byte[])ip.getPixels();
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		
+		int[][] inArr = ImageJUtility.convertFrom1DByteArr(pixels, width, height);
+		
+		int threshold = 127;
+		int BG_VAL = 0;
+		int FG_VAL = 255;
+		
+		int[][] outArr = new int[width][height];
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y <height; y++){
+				if(inArr[x][y] < threshold){
+					outArr[x][y] = FG_VAL;
+				}
+				else{
+					outArr[x][y] = BG_VAL;
+				}
+			}
+		}
+		
+		
+		
+		byte[] outPixels = ImageJUtility.convertFrom2DIntArr(inArr, width, height);
+		ImageJUtility.showNewImage(outPixels, width, height, "threshold with T= " + threshold);
+		
+	} //run
+
+	void showAbout() {
+		IJ.showMessage("About Template_...",
+			"this is a PluginFilter template\n");
+	} //showAbout
+
+}
