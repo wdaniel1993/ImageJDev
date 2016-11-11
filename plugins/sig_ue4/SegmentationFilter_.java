6972b8d3e6579c0fe946febbe09bb058da097e23
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-11-11 20:04:38 +0100

    Ue4

---
+++ plugins/sig_ue4/SegmentationFilter_.java
@@ -0,0 +1,19 @@
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class SegmentationFilter_ implements PlugInFilter {
+
+	@Override
+	public void run(ImageProcessor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int setup(String arg0, ImagePlus arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
