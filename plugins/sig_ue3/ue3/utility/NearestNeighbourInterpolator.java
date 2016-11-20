6622b0346cbe615e4d54d6b3bedcbc9cbb73382e
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-11-20 18:30:58 +0100

    Correct NearestNeighbour

--- plugins/sig_ue3/ue3/utility/NearestNeighbourInterpolator.java
+++ plugins/sig_ue3/ue3/utility/NearestNeighbourInterpolator.java
@@ -6,10 +6,9 @@ public class NearestNeighbourInterpolator extends Interpolator {
 
 	@Override
 	public int getInterpolatedValue(Image2D image, double posX, double posY){
-		if((posX >= 0) && (posY >= 0) && (posX < image.getWidth()) && (posY < image.getHeight())) {
-			int indexX = (int) Math.round(posX);
-			int indexY = (int) Math.round(posY);
-			
+		int indexX = (int) Math.round(posX);
+		int indexY = (int) Math.round(posY);
+		if((indexX >= 0) && (indexY >= 0) && (indexX < image.getWidth()) && (indexY < image.getHeight())) {
 			return image.get(indexX, indexY);
 		} else {
 			return 255;
