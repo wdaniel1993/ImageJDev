cea9646cb8e6b9c977635ce58b9894b68cd5c84e
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-11-25 18:23:12 +0100

    Region Growing

--- plugins/sig_ue4/RegionGrowing_.java
+++ plugins/sig_ue4/RegionGrowing_.java
@@ -1,3 +1,7 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.Stack;
+
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
@@ -19,18 +23,45 @@ public class RegionGrowing_ implements PlugInFilter {
 		
 		int[][] inArr = ImageJUtility.convertFrom1DByteArr(pixels, width, height);
 		
-		int threshold = 127;
+		int lowerThresh = 80;
+		int upperThresh = 120;
 		int BG_VAL = 0;
 		int FG_VAL = 255;
 		
 		int[][] outArr = new int[width][height];
+		
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y <height; y++){
-				if(inArr[x][y] < threshold){
-					outArr[x][y] = FG_VAL;
-				}
-				else{
-					outArr[x][y] = BG_VAL;
+				outArr[x][y] = BG_VAL;
+			}
+		}
+		
+		//user defined seed position
+		Rectangle rect = ip.getRoi();
+		int startX = rect.x + rect.width / 2;
+		int startY = rect.y + rect.height / 2;
+		
+		Stack<Point> stack = new Stack<Point>();
+		stack.push(new Point(startX, startY));
+		outArr[startX][startY] = FG_VAL;
+		
+		while(!stack.empty()){
+			Point curPosition = stack.pop();
+			
+			//search neighbours
+			for(int xOffset = -1; xOffset <= 1; xOffset++){
+				for(int yOffset = -1; yOffset <= 1; yOffset++){
+					int nbX = curPosition.x + xOffset;
+					int nbY = curPosition.y + yOffset;
+					
+					//range check
+					if(nbX >= 0 && nbX < width && nbY >= 0 && nbY < height){
+						int nbVal = inArr[nbX][nbY];
+						if(nbVal >= lowerThresh && nbVal <= upperThresh && outArr[nbX][nbY] == BG_VAL){
+							outArr[nbX][nbY] = FG_VAL;
+							stack.push(new Point(nbX,nbY));
+						}
+					}
 				}
 			}
 		}
@@ -38,7 +69,7 @@ public class RegionGrowing_ implements PlugInFilter {
 		
 		
 		byte[] outPixels = ImageJUtility.convertFrom2DIntArr(outArr, width, height);
-		ImageJUtility.showNewImage(outPixels, width, height, "threshold with T= " + threshold);
+		ImageJUtility.showNewImage(outPixels, width, height, "region growing (lower thresh = " + lowerThresh + ", upper thresh = " + upperThresh);
 		
 	} //run
 
